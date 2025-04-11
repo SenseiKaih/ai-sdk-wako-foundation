e711ffb74eae841a43fbb0cc77e6fd51b5e87362
kyarmin nalawala <808072@columbiacollege.ca>
2025-04-11 19:54:42 +0300

    Update project-overview.tsx

--- components/project-overview.tsx
+++ components/project-overview.tsx
@@ -1,10 +1,9 @@
-import NextLink from "next/link";
 export const ProjectOverview = () => {
   return (
     <div className="flex flex-col items-center justify-end">
       <h1 className="text-3xl font-semibold mb-4">Prince Wako Foundation Chatbot</h1>
       <p className="text-center max-w-xl">
-        Welcome to the Prince Wako Foundation's AI Chatbot! This assistant
+        Welcome to the Prince Wako Foundation&apos;s AI Chatbot! This assistant
         helps you explore our initiatives, generate content, and learn more about our
         mission to support communities with better access to clean water, education, and
         opportunity.
